4fa0abed017b882975d37551df62b47cb58fd0df
Loganathan Murugesan <cresloga@gmail.com>
2019-09-18 23:46:09 +0530

    adding loggers

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const awsS3Config =
 
 exports.handler = function(event, context,callback) {
 	var picIndex = event.pathParameters.picIndex;
+	console.log("picIndex Received: "+picIndex);
 	const s3 = new S3(awsS3Config);
 	var params = {Bucket: S3_BUCKET};
 
@@ -17,22 +18,27 @@ exports.handler = function(event, context,callback) {
 
 	s3.listObjects(params, function(err, data){
 		if(err){
+			console.log("Error listing objects: "+err);
 			responseBody = err;
 			responseStatus = 417;
 		}
 		else{
+			console.log("List of Objects: "+data);
 			var bucketContents = data.Contents;
 			if(picIndex<0) picIndex = bucketContents.length-1;
-			else if (picIndex>=bucketContents.length) picIndex = 0;
-			//console.log("Bucket Size :"+bucketContents.length);			
+			else if (picIndex>=bucketContents.length) picIndex = 0;		
+			console.log("picIndex to be used: "+picIndex);
+			console.log("bucket size: "+bucketContents.length);
 			if(picIndex<bucketContents.length){
 				var urlParams = {Bucket: S3_BUCKET, Key: bucketContents[picIndex].Key};
 				s3.getSignedUrl('getObject',urlParams, function(err, url){
 					if(err){
+						console.log("Error getting signedUrl : "+err);
 						responseBody = err;
 						responseStatus = 417;
 					}
 					else{
+						console.log("URL : "+url);
 						responseBody = {
 							url: url,
 							picIndex: picIndex,
@@ -51,7 +57,7 @@ exports.handler = function(event, context,callback) {
 			"body": JSON.stringify(responseBody),
 			"isBase64Encoded": false
 		}
-
+		console.log(response);
 		callback(null,response);		
 	});
 }
